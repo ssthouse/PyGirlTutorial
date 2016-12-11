14401d3f151426c0d081c06d994a9a37b5d59ff7
ssthouse <ssthouse@163.com>
2016-12-11 19:16:51 +0800

    Add new post logic

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    print("**************************")
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    form = PostForm()
+    print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+    return render(request, 'blog/post_edit.html', {'form': form})
